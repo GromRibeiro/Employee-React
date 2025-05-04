58aedf4d80818634f066231610742c309156b6bd
Gabriel Ribeiro <89033176+GromRibeiro@users.noreply.github.com>
2025-05-04 16:56:33 -0300

    update home add employee component

--- employee/src/pages/Home.tsx
+++ employee/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import EmployeeForm from "../components/EmployeeForm";
+import EmployeeTable from "../components/EmployeeTable";
 
 
 export default function Home() {
@@ -6,6 +7,9 @@ export default function Home() {
     <div className="min-h-screen w-full bg-gray-100 p-6 flex flex-col items-center">
       <h1 className="text-3xl text-black font-bold mb-6">Gestão de Funcionários e IRRF</h1>
       <EmployeeForm />
+      <div className="mt-8 w-full ">
+        <EmployeeTable />
+      </div>
     </div>
   );
 }
